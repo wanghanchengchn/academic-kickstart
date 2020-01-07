9eff53681f5033cc7009d6e0dc6fa941d1a3e2f3
w13356612981 <w13356612981@163.com>
2020-01-07 23:07:47 +0800

    Initial commit

---
+++ update.sh
@@ -0,0 +1,11 @@
+rm -r public/
+git submodule add -f -b master https://github.com/wanghanchengchn/wanghanchengchn.github.io.git public
+git add .
+git commit -m "Initial commit"
+git push -u origin master
+hugo
+cd public
+git add .
+git commit -m "Build website"
+git push origin master
+cd ..
